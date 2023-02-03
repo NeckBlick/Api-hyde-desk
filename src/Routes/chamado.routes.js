c7cf0ca2c613a727f406e41cb6435e3d351c29e6
VitorPerrone <vitorperrone2005@gmail.com>
2023-02-03 07:34:06 -0300

    get chamados

--- src/Routes/chamado.routes.js
+++ src/Routes/chamado.routes.js
@@ -1,7 +1,28 @@
 const express = require("express")
+const db = require("../../conexao")
 
 const routes = express.Router()
+//criando um get para o chamados
+routes.get("/", (req, res, next) => {
+    db.getConnection((error, conn) => {
 
+      conn.query(
+        "SELECT * FROM chamados",
+  
+        (error, result, field) => {
+          conn.resume();
+  
+          if (error) {
+            res.status(500).send({
+              error: error,
+              response: null,
+            });
+          }
+          res.status(200).send(result);
+        }
+      );
+    });
+  });
 
 
 module.exports = routes
