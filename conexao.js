cb6c2246584842192cdd8867679e94545d352d0c
NeckBlick <nickmestre200@gmail.com>
2023-02-08 14:58:42 -0300

    ssl

--- conexao.js
+++ conexao.js
@@ -11,15 +11,11 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
-<<<<<<< HEAD
   database: process.env.DB_NAME,
   ssl:{
     rejectUnauthorized: true,
     ca: serverca
   }
-=======
-  database: process.env.DB_DATABASE
->>>>>>> 958c9ad46dc4f1ddfab875132015d4bd6f1fdcd1
 });
 
 module.exports = db;
