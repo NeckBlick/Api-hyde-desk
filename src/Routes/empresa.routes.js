63eb36e825f225695ea2d083e68913269de1808d
NeckBlick <nickmestre200@gmail.com>
2023-03-01 11:10:48 -0300

    Implementação do email

--- src/Routes/empresa.routes.js
+++ src/Routes/empresa.routes.js
@@ -123,7 +123,7 @@ routes.post("/cadastro", upload.single('foto'), async (req, res, next) => {
                   var jsonData = {
                     toemail: email,
                     nome: nome,
-                    tipo: cadastro
+                    tipo: "cadastro"
                   };
                   const response = await axios.post("https://prod2-16.eastus.logic.azure.com:443/workflows/84d96003bf1947d3a28036ee78348d4b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5BhPfg9NSmVU4gYJeUVD9yqkJPZACBFFxj0m1-KIY0o", jsonData);
                   if(response.status == 200){
