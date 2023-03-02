5d8e91c7650b073ab7149007f49ae15bed348085
Leonardo Lopes <105213396+LeoLopesRibeiro@users.noreply.github.com>
2023-03-02 08:26:45 -0300

    Update email.routes.js

--- src/Routes/email.routes.js
+++ src/Routes/email.routes.js
@@ -39,7 +39,7 @@ routes.post("/", async (req, res) => {
 
   try {
     const response = await axios.post("https://prod2-16.eastus.logic.azure.com:443/workflows/84d96003bf1947d3a28036ee78348d4b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5BhPfg9NSmVU4gYJeUVD9yqkJPZACBFFxj0m1-KIY0o", jsonData);
-    return res.status(201).send({menssage: "Email enviado com sucesso!",})
+    return res.status(201).send({menssage: "Email enviado com sucesso!", token: token})
     console.log(response.status);
   } catch (error) {
     console.log(error);
