be2a8fa7f810803e1c687dd299f9f99cc3344c1d
Leonardo Lopes <leolopesribeiro05@gmail.com>
2023-03-07 17:32:08 -0300

    Status de erro arrumado

--- src/Routes/email.routes.js
+++ src/Routes/email.routes.js
@@ -84,17 +84,17 @@ routes.post("/", async (req, res) => {
           try {
             async function enviarEmail(){
               const response = await axios.post("https://prod2-16.eastus.logic.azure.com:443/workflows/84d96003bf1947d3a28036ee78348d4b/triggers/manual/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=5BhPfg9NSmVU4gYJeUVD9yqkJPZACBFFxj0m1-KIY0o", jsonData);
-              return res.status(201).send({ menssage: "Email enviado com sucesso!", token: token })
+              return res.status(201).send({ message: "Email enviado com sucesso!", token: token })
               console.log(response.status);
 
             }enviarEmail()
           } catch (error) {
             console.log(error);
-            return res.status(401).send({ menssage: error })
+            return res.status(401).send({ message: error })
           }
         }
       } else {
-        return res.status(204).send({ message: error, })
+        return res.status(404).send({ message: error, })
       }
     })
   })
