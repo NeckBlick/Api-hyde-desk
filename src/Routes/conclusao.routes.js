b91a4d627467f9e60fd1f20547622bbe195b312a
Breno-S <114250412+Breno-S@users.noreply.github.com>
2023-05-22 11:39:56 -0300

    Arrumando conclusões e rota de avaliar

--- src/Routes/conclusao.routes.js
+++ src/Routes/conclusao.routes.js
@@ -44,15 +44,10 @@ routes.get("/", login, (req, res, next) => {
       });
     }
 
-    let query = "SELECT * FROM conclusoes";
+    let query =
+      "SELECT *, con.descricao AS descricao_conclusao, con.anexo AS anexo_conclusao FROM conclusoes AS con INNER JOIN chamados AS c ON con.chamado_id = c.id_chamado INNER JOIN funcionarios AS f ON c.funcionario_id = f.id_funcionario INNER JOIN tecnicos AS t ON c.tecnico_id = t.id_tecnico INNER JOIN empresas AS e ON e.id_empresa = f.empresa_id";
 
     let keysFilters = Object.keys(filters);
-
-    keysFilters.forEach((key) => {
-      if (key === "id_empresa") {
-        query = `SELECT * FROM conclusoes AS con INNER JOIN chamados AS c ON con.chamado_id = c.id_chamado INNER JOIN funcionarios AS f ON c.funcionario_id = f.id_funcionario INNER JOIN empresas AS e ON e.id_empresa = f.empresa_id`;
-      }
-    });
 
     if (keysFilters.length !== 0) {
       query += " WHERE";
@@ -60,7 +55,7 @@ routes.get("/", login, (req, res, next) => {
       try {
         keysFilters.forEach((key, index) => {
           if (index !== keysFilters.length - 1) {
-            console.log(filters)
+            console.log(filters);
             query += ` ${key} LIKE '${filters[key]}' AND`;
           } else {
             query += ` ${key} LIKE '${filters[key]}'`;
@@ -74,9 +69,8 @@ routes.get("/", login, (req, res, next) => {
       }
     }
 
-    console.log(query);
-
     conn.query(query, (error, results, fields) => {
+      conn.release();
       if (error) {
         return res.status(500).send({
           message: "Não foi possível consultar conclusões.",
@@ -84,7 +78,21 @@ routes.get("/", login, (req, res, next) => {
         });
       }
 
-      return res.status(200).send(results);
+      if (results.length === 0) {
+        return res.status(200).send([])
+      }
+
+      return res.status(200).send([{
+        id_conclusao: results[0].id_conclusao,
+        descricao: results[0].descricao_conclusao,
+        anexo: results[0].anexo_conclusao,
+        data_termino: results[0].data_termino,
+        num_avaliacao: results[0].num_avaliacao,
+        desc_avaliacao: results[0].desc_avaliacao,
+        chamado_id: results[0].chamado_id,
+        nome_empresa: results[0].nome_empresa,
+        nome_tecnico: results[0].nome_tecnico,
+      }]);
     });
   });
 });
@@ -127,6 +135,7 @@ routes.get("/:id_conclusao", login, (req, res, next) => {
     const query = "SELECT * FROM conclusoes WHERE id_conclusao = ?";
 
     conn.query(query, [id_conclusao], (error, result, fields) => {
+      conn.release();
       if (error) {
         return res.status(500).send({
           message: "Não foi possível consultar conclusão.",
